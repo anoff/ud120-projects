234fec9b462e94587c42c4095a573ce02fda5b13
anoff <offenhaeuser@gmail.com>
2017-03-18 15:11:24 +0100

    :star2: feat: decision tree example

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,8 +24,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+t1 = time()
+acc = clf.score(features_test, labels_test)
+t2 = time()
+print("prediction accuracy: {}".format(acc))
+print("training time: {0}s ({1} data points), prediction time: {2}s ({3} data points)".format(round(t2-t0, 3), len(features_train), round(t2-t1, 3), len(features_test)))
 #########################################################
 
 
