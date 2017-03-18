f024ec8b71950c2aecb85167a942f9093b253b3a
anoff <offenhaeuser@gmail.com>
2017-03-18 11:06:18 +0100

    :star2: feat: svm full set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
+import sys
+from time import time
+from sklearn import svm
+sys.path.append("../tools/")
+from email_preprocess import preprocess
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
     
-import sys
-from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+
 
 
 ### features_train and features_test are the features for the training
@@ -25,6 +27,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# decrease training set size
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf = svm.SVC(C=1.0, kernel='linear')
+clf.fit(features_train, labels_train)
+t1 = time()
+print("prediction accuracy: {}".format(clf.score(features_test, labels_test)))
+t2 = time()
+print("training time: {0}s ({1} data points), prediction time: {2}s ({3} data points)".format(round(t2-t0, 3), len(features_train), round(t2-t1, 3), len(features_test)))
 #########################################################
 
 
